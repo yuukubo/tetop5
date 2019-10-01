30a01c1b5ac12b9db4555572956b820988f8c54e
yuukubo <i.yusuke.shk@gmail.com>
2019-10-01 23:06:14 +0900

    key test

--- sketch.js
+++ sketch.js
@@ -1,15 +1,17 @@
 // teto
 
 let [canvasx, canvasy] = [720, 690];
+let [canvasFromx, canvasFromy] = [0, 0];
 let gameage = 0;
 let blockwh = 20;
-let blockspd = 1;
+let blockspdy = 1;
+let blockspdx = 3;
 let blockx = 0;
 let blocky = 0;
 
 function setup() {
   createCanvas(canvasx, canvasy);
-  blockx = canvasx - blockwh;
+  blockx = (canvasx - blockwh) / 2;
   blocky = 0;
 }
 
@@ -17,11 +19,32 @@ function draw() {
   background(0);
   strokeWeight(2);
   stroke(100);
-  rect((blockx) / 2, blocky, blockwh, blockwh);
-  blocky += blockspd;
+  rect(blockx, blocky, blockwh, blockwh);
+  blocky += blockspdy;
+
+  if (keyIsDown(LEFT_ARROW)) {
+    if (canvasFromx < blockx) {
+      blockx -= blockspdx;
+    }
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    if (blockx < canvasx) {
+      blockx += blockspdx;
+    }
+  }
+  // if (keyIsDown(UP_ARROW)) {
+  //   if (canvasFromy < blocky) {
+  //     blocky -= blockspdy;
+  //   }
+  // }
+  if (keyIsDown(DOWN_ARROW)) {
+    if (blocky < canvasy) {
+      blocky += blockspdy;
+    }
+  }
 
   if (canvasx <= blockx + blockwh) { blockx = canvasx - blockwh }
-  if (blockx <= 0) { blockx = 0  }
+  if (blockx <= canvasFromx) { blockx = canvasFromx  }
   if (canvasy <= blocky + blockwh) { blocky = canvasy - blockwh }
-  if (blocky < 0) { blocky = 0 }
+  if (blocky < canvasFromy) { blocky = canvasFromy }
 }

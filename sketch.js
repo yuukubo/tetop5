c2f6261d77edc6ab2c18d7fe5cf2175431884d33
yuukubo <i.yusuke.shk@gmail.com>
2019-10-04 22:19:34 +0900

    grid test.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 // teto
 
-let [canvasx, canvasy] = [720, 690];
+let [canvasx, canvasy] = [700, 680];
 let [canvasFromx, canvasFromy] = [0, 0];
 let gameage = 0;
 let blockwh = 20;
@@ -9,19 +9,42 @@ let blockspdx = blockwh;
 let blockx = 0;
 let blocky = 0;
 let keyInterval = 0;
+let linexfr = 0;
+let lineyfr = 0;
+let linexto = canvasx;
+let lineyto = canvasy;
+let linexinterval = 20;
+let lineyinterval = 20;
+let linexnum = 0;
+let lineynum = 0;
 
 function setup() {
   createCanvas(canvasx, canvasy);
-  blockx = canvasx / 2;
   blocky = 0;
+  linexnum = canvasx / linexinterval;
+  lineynum = canvasy / lineyinterval;
+  blockx = floor(linexnum / 2) * blockwh;
 }
 
 function draw() {
   background(0);
+
+  push();
+  stroke(255, 100);
+  for (let i = 0; i < lineynum; i++) {
+    line(linexfr, lineyfr + i * lineyinterval, linexto, lineyfr + i * lineyinterval);  
+  }
+  for (let i = 0; i < linexnum; i++) {
+    line(linexfr + i * linexinterval, lineyfr, linexfr + i * linexinterval, lineyto);  
+  }
+  pop();
+
+  push();
   strokeWeight(2);
   stroke(100);
   rect(blockx, blocky, blockwh, blockwh);
   blocky += blockspdy;
+  pop();
 
   if (0 < keyInterval) {
     keyInterval--;
